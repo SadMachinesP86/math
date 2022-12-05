a35583f28a046a8aea20b3e232efa0c7f059a5d0
Derek Hockett <dhockett@dmp.com>
2022-12-04 22:44:53 -0600

    Automagically define methods for FFI calls

--- lib/geometry/rectangle.rb
+++ lib/geometry/rectangle.rb
@@ -1,29 +1,10 @@
 require_relative 'ffi/rectangle'
+require_relative 'rusty'
 
 module Geometry
   class Rectangle
-    def initialize(width, height)
-      @ptr = FFI::Rectangle.rectangle_make(width, height)
-    end
+    extend Rusty
 
-    def area
-      FFI::Rectangle.rectangle_area(@ptr)
-    end
-
-    def width
-      FFI::Rectangle.rectangle_get_width(@ptr)
-    end
-
-    def width=(width_to_set)
-      FFI::Rectangle.rectangle_set_width(@ptr, width_to_set)
-    end
-
-    def height
-      FFI::Rectangle.rectangle_get_height(@ptr)
-    end
-
-    def height=(height_to_set)
-      FFI::Rectangle.rectangle_set_height(@ptr, height_to_set)
-    end
+    exposes 'Rectangle'
   end
 end
